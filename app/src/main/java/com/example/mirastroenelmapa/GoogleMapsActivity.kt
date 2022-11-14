1eb5e6a87b1d3d4eb600045894e30a67ddde9d04
Leandro <leo250703@gmail.com>
2022-11-14 10:58:08 -0400

    Commit 14/11/2022 10:57 (Leandro Morales) -> Cambios en GoogleMapsActivity

--- app/src/main/java/com/example/mirastroenelmapa/GoogleMapsActivity.kt
+++ app/src/main/java/com/example/mirastroenelmapa/GoogleMapsActivity.kt
@@ -1,7 +1,16 @@
 package com.example.mirastroenelmapa
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,8 +19,30 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.mirastroenelmapa.databinding.ActivityGoogleMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
 
 class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    companion object {
+        val REQUIERED_PERMISSION_GPS = arrayOf(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_NETWORK_STATE
+        )
+    }
+
+    private var isGPSEnabled = false
+    private val PERMISSION_ID = 42
+    //Variable que vamos a usar para gestionar el GPS con google play services
+    //FusedLocation: fusionar los datos respectivos a GPS en un objeto
+    private lateinit var fusedLocation : FusedLocationProviderClient
+
+    private var latitud: Double = 0.0
+    private var longitud: Double = 0.0
+    private var distance: Double = 0.0
+    private var acumulateDistance: Double = 0.0
+    private var velocity: Double = 0.0
+    private var contador = 0
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityGoogleMapsBinding
@@ -26,6 +57,13 @@ class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        binding.btnHabilitarGPS.setOnClickListener {
+
+        }
+        binding.btnHabilitarCoor.setOnClickListener {
+
+        }
     }
 
     //Función para cuando el mapa este listo
@@ -51,4 +89,58 @@ class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
             isZoomControlsEnabled = false // deshabilita las opciones de zoom con los dedos del mapa
         }
     }
+
+
+    //HABILITAR PERMISOS DE GPS Y COORDENADAS
+
+    /**
+     *
+     *
+     * GPS
+     *
+     */
+
+    private fun enableGPSServices() {
+        if(!hasGPSEnabed()){
+            AlertDialog.Builder(this)
+                .setTitle(com.google.android.gms.location.R.string.alert_dialog_title)
+                .setMessage(com.google.android.gms.location.R.string.alert_dialog_description)
+                .setPositiveButton(
+                    com.google.android.gms.location.R.string.alert_dialog_button_accept,
+                    DialogInterface.OnClickListener{
+                            dialog, wich -> goToEnableGPS()
+                    })
+                .setNegativeButton(com.google.android.gms.location.R.string.alert_dialog_button_denny) {
+                        dialog, wich -> isGPSEnabled = false
+                }
+                .setCancelable(true)
+                .show()
+        } else
+            Toast.makeText(this,"Ya tienes el GPS habilitado", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun hasGPSEnabed(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun goToEnableGPS() {
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivity(intent)
+    }
+
+    //Seccion: tratamiento de permisos para el uso del GPS
+    private fun allPermissionsGrantedGPS(): Boolean {
+        return REQUIERED_PERMISSION_GPS.all {
+            ContextCompat.checkSelfPermission( baseContext, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    /**
+     *
+     *
+     * Coordenadas
+     *
+     * */
 }
